2df48e59382ece1e4171ec94c2b5ee0600355d4a
theo boucher <theoboucher@yahoo.fr>
2020-02-15 20:26:47 +0100

    envoi plan menu1

--- src/structure.h
+++ src/structure.h
@@ -3,16 +3,32 @@
 
 #include <SANDAL2/SANDAL2.h>
 
-#define ECRAN_TITRE 0
 #define LFEN 1400
 #define HFEN 800
 
-enum player {
+enum Player {
 	JOUEUR_G = 0,
 	JOUEUR_D = 1
 };
 
-enum NumPlan {
+enum Display {
+	ECRAN_TITRE = 0,
+	ECRAN_CHOIX_PERSO = 1,
+	ECRAN_CHOIX_MAP = 2,
+	ECRAN_FIGHT = 3
+}
+
+// liste des plans du menu
+enum PlanMenu {
+  //1 ECRAN_TITRE
+	PlanTitre = 8,
+	PlanStart = 6,
+	PlanAnim = 5,
+	PlanRideau = 4
+}
+
+// liste des plans du fight
+enum {
 	PlanDecor = 10,
 	PlanCharacter = 0,
 };
