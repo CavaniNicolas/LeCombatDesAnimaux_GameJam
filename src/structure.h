1497142be52f5607920d6e5cced83db834b9ea20
theo boucher <theoboucher@yahoo.fr>
2020-02-16 01:28:08 +0100

    envoi struct

--- src/structure.h
+++ src/structure.h
@@ -5,6 +5,7 @@
 
 #define LFEN 1400
 #define HFEN 800
+#define NBPERSO 4
 
 enum Player {
 	JOUEUR_G = 0,
@@ -21,20 +22,43 @@ enum Display {
 // liste des plans du menu
 enum PlanMenu {
   //1 ECRAN_TITRE
-	PlanTitre = 8,
-	PlanStartUp = 7,
-	PlanStartDown = 6,
-	PlanAnim = 5,
-	PlanRideau = 4
+	PlanTitreFond = 15,
+	PlanTitre = 14,
+	PlanStartUp = 8,
+	PlanStartDown = 7,
+	//PlanAnim = 6,
+	PlanRideau = 5,
+
+	//2 ECRAN_CHOIX_PERSO
+	PlanContourNoir = 13,
+	PlanContourBlanc = 12,
+	PlanFondNoir = 11,
+	PlanBtnRetourUp = 10,
+	PlanBtnRetourDown = 9,
+	PlanPerso = 8,
+	PlayerContourPersoUp = 7,
+	PlayerContourPersoDown = 6,
+	PlanInfo = 5,
+	PlanInfoModif = 4,
+	PlanBtnValiderUp = 3,
+	PlanBtnValiderDown = 2
 };
 
 typedef struct DataStart {
 	int verif;
 }DataStart;
 
+typedef struct DataValidate {
+	int verif;
+}DataValidate;
+
+typedef struct DataRetour {
+	int verif;
+}DataRetour;
+
 // liste des plans du fight
 enum {
-	PlanDecor = 10,
+	PlanDecor = 20,
 	PlanCharacter = 0,
 };
 
