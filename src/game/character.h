acc5ca43c438b7dcb452efe840a7d9a76bb77953
Cavani Nicolas <cavani.nicolas6@gmail.com>
2020-04-21 15:59:57 +0200

    add documentation

--- src/game/character.h
+++ src/game/character.h
@@ -1,15 +1,38 @@
+/* -------------------------------------------------------------------------- */
+/* Nom du fichier : character.h                                               */
+/* Objectifs : Déclarations des fonctions pour le personnage                  */
+/* Fonctions :                                                                */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/*  -                                             */
+/* -------------------------------------------------------------------------- */
 
 #ifndef CARA_H
 #define CARA_H
 #include <stdbool.h>
 
+/* -------------------------------------------------------------------------- */
+/* struct HealthBar (typedef HealthBar_t)                                     */
+/*       healthBar : pointeur sur la barre de vie                             */
+/*       bubble1   : pointeur sur la bulle de victoire 1                      */
+/*       bubble2   : pointeur sur la bulle de victoire 2                      */
+/* -------------------------------------------------------------------------- */
 typedef struct HealthBar {
 	Element * healthBar;
 	Element * bubble1;
 	Element * bubble2;
 }HealthBar_t;
 
-
+/* -------------------------------------------------------------------------- */
+/* struct KeyCodes (typedef KeyCodes_t)                                       */
+/*       left, right, jump, attack1, attack2, parry                           */
+/* -------------------------------------------------------------------------- */
 typedef struct KeyCodes {
 	SDL_Keycode left;
 	SDL_Keycode right;
@@ -19,6 +42,11 @@ typedef struct KeyCodes {
 	SDL_Keycode parry;
 }KeyCodes_t;
 
+/* -------------------------------------------------------------------------- */
+/* struct KeyPressed (typedef KeyPressed_t)                                   */
+/*  - Booléens, true si la touche est appuyée false si elle est relachée      */
+/*       left_P, right_P, jump_P, attack1_P, attack2_P, parry_P               */
+/* -------------------------------------------------------------------------- */
 typedef struct KeyPressed {
 	bool left_P;
 	bool right_P;
@@ -28,6 +56,11 @@ typedef struct KeyPressed {
 	bool attack2_P;
 }KeyPressed_t;
 
+/* -------------------------------------------------------------------------- */
+/* struct DataCharacter (typedef DataCharacter_t)                             */
+/*  - accessible depuis l'element character->data                             */
+/*    regroupe toutes les données d'un personnage                             */
+/* -------------------------------------------------------------------------- */
 typedef struct DataCharacter {
 	int    idChara;
 	int    width;
@@ -69,76 +102,139 @@ typedef struct DataCharacter {
 	KeyCodes_t * keyCodes;
 }DataCharacter_t;
 
-/* ------------------------------------------------------------------------- */
-/* keyPressed                                                                */
-/*       Active les booléens de pression en fonction de la touche appuyée    */
-/*                                                                           */
-/* En entrée:  character : Element *, personnage                             */
-/*             k         : SDL_Keycode, touche clavier (pression utilisée)   */
-/*                                                                           */
-/* En sortie:  void                                                          */
-/* ------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* keyPressed                                                                 */
+/*       Active les booléens de pression en fonction de la touche appuyée     */
+/*                                                                            */
+/* En entrée: character (Element *) : personnage                              */
+/*            k  (SDL_Keycode)      : touche clavier (pression utilisée)      */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void keyPressed(Element *, SDL_Keycode);
 
-/* -------------------------------------------------------------------------------- */
-/* keyUnpressed Desactive les booléens de pression en fonction de la touche relachée*/
-/*                                                                                  */
-/* En entrée:  character : Element *, personnage                                    */
-/*             k         : SDL_Keycode, touche clavier (relachement utilisé)        */
-/*                                                                                  */
-/* En sortie:  void                                                                 */
-/* -------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* keyUnpressed                                                               */
+/*       Desactive les booléens de pression en fonction de la touche relachée */
+/*                                                                            */
+/* En entrée: character (Element *) : personnage                              */
+/*            k  (SDL_Keycode)      : touche clavier (relachement utilisé)    */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void keyUnpressed(Element *, SDL_Keycode);
 
 
+/* -------------------------------------------------------------------------- */
+/* keyOnActions                                                               */
+/*       Actions à effectuer en fonctions des touches qui sont appuyées       */
+/*                                                                            */
+/* En entrée: character (Element *) : personnage                              */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void keyOnActions(Element *);
+
+
+/* -------------------------------------------------------------------------- */
+/* keyOffActions                                                              */
+/*       Actions à effectuer en fonctions des touches qui sont relachées      */
+/*                                                                            */
+/* En entrée: character (Element *) : personnage                              */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void keyOffActions(Element *);
+
+
+/* -------------------------------------------------------------------------- */
+/* actionCharacters                                                           */
+/*       Actions à appliquer en permanence au personnage                      */
+/*                                                                            */
+/* En entrée: character (Element *) : personnage                              */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void actionCharacters(Element * character);
 
 
-/* -------------------------------------------------------------------------------- */
-/* initCharacter      Initialise le personnage                                      */
-/*                                                                                  */
-/* En entrée:  idPlayer : int, id du joueur choisissant son perso (gauche ou droite)*/
-/*             idChara  : int, id du perso selectionné dans le menu                 */
-/*             character: Element ** , personnage                                   */
-/*                                                                                  */
-/* En sortie:  void                                                                 */
-/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* initCharacter                                                              */
+/*       Initialise le personnage                                             */
+/*                                                                            */
+/* En entrée: idPlayer (int) : id du joueur choisissant son perso             */
+/*                             (gauche ou droite)                             */
+/*            idChara  (int) : id du perso selectionné dans le menu           */
+/*            character (Element **) : personnage                             */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void initCharacter(int, int, Element **);
 
 
 
-/* -------------------------------------------------------------------------------- */
-/* initDataCharacter   Initialise les data d'un personnage depuis le fichier data   */
-/*                     "assets/DataCharacters.txt"                                  */
-/*                                                                                  */
-/* En entrée:  idPlayer : int, id du joueur choisissant son perso (gauche ou droite)*/
-/*             idChosen : int, id du perso selectionné dans le menu                 */
-/*                                                                                  */
-/* En sortie:  DataCharacter_t * :   Pointeur sur la structure Data du perso        */
-/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* initDataCharacter                                                          */
+/*                                                                            */
+/*       Initialise les data d'un personnage depuis le fichier data           */
+/*       "assets/DataCharacters.txt"                                          */
+/*                                                                            */
+/* En entrée: idPlayer (int) : id du joueur choisissant son perso            */
+/*                             (gauche ou droite)                            */
+/*            idChosen (int) : id du perso selectionné dans le menu          */
+/*                                                                            */
+/* En sortie: d (DataCharacter_t *) : Pointeur sur la structure Data du perso*/
+/* -------------------------------------------------------------------------- */
 DataCharacter_t * initDataCharacter(int, int);
 
 
-//associe les touches claviers au bon personnage
+/* -------------------------------------------------------------------------- */
+/* initKeyCodes                                                               */
+/*       Remplie la structure KeyCodes avec les touches claviers              */
+/*       correspondantes au personnage du joueur idPlayer                     */
+/*                                                                            */
+/* En entrée: kd (KeyCodes_t *) : pointeur sur la structure KeyCodes          */
+/*            idPlayer (int)    : id du joueur choisissant son perso          */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void initKeyCodes(KeyCodes_t * kd, int idPlayer);
 
-//init les KeyPressed a false
+
+/* -------------------------------------------------------------------------- */
+/* initKeyPressed                                                             */
+/*       Initialise les booleens de la structure KeyPressed à false           */
+/*                                                                            */
+/* En entrée: kp (KeyPressed_t *) : pointeur sur la structure KeyPressed      */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void initKeyPressed(KeyPressed_t * kp);
 
-// affiche les barres de vie initiales
+
+/* -------------------------------------------------------------------------- */
+/* initHealthBar                                                              */
+/*       Initialise les barres de vie, affichage (barres, bulles de victoire, */
+/*       logo du perso), pointeur sur la structure                            */
+/*                                                                            */
+/* En entrée: hb (HealthBar_t *) : pointeur sur la structure HealthBar        */
+/*                                                                            */
+/* En sortie: void                                                            */
+/* -------------------------------------------------------------------------- */
 void initHealthBar(HealthBar_t * hb, int idPlayer);
 
 
-/* -------------------------------------------------------------------------------- */
-/* getCharactersFilename   Récupère le nom du fichier de l'image du perso choisie   */
-/*                                                                                  */
-/* En entrée:  idChara  : int, perso choisi                                         */
-/*             filename : char[40], nom du fichier de l'image du perso choisi       */
-/*                                                                                  */
-/* En sortie:  int, 1 si pas d'erreurs, 0 sinon                                     */
-/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* getCharactersFilename                                                      */
+/*       Récupère le nom du fichier de l'image du perso choisie               */
+/*                                                                            */
+/* En entrée: idChara  (int)      : perso choisi                              */
+/*            filename (char[40]) : nom du fichier de l'image du perso choisi */
+/*                                                                            */
+/* En sortie: int, 1 si pas d'erreurs, 0 sinon                                */
+/* -------------------------------------------------------------------------- */
 int getCharactersFilename(int, char[40]);
 
 
